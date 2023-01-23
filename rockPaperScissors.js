60e3574ef9c387518a7236a7805265a8e0d970bc
DanielKirilov <dancata08@gmail.com>
2023-01-23 14:29:20 +0200

    completed the game
    the computer chooses random move and the console lets you know what's the result of the game!

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,8 +1,10 @@
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
-let playerTurn;
-let computerTurn
+
+let computerTurn;
+let pcRandomNumber = Math.floor(Math.random() * 3) + 1;
+let playerTurn = paper;
 
 if (playerTurn === "r" || playerTurn === rock) {
     playerTurn = rock;
@@ -13,10 +15,56 @@ if (playerTurn === "r" || playerTurn === rock) {
 } else {
     console.log("Invalid input. Try again!");
 }
-let pcRandomNumber = Math.floor(Math.random() * 3) + 1;
 
-switch(pcRandomNumber) {
-    case 1: computerTurn = "rock"; break;
-    case 2: computerTurn = "paper"; break;
-    case 3: computerTurn = "scissors"; break;
-} 
+switch (pcRandomNumber) {
+    case 1: computerTurn = rock; break;
+    case 2: computerTurn = paper; break;
+    case 3: computerTurn = scissors; break;
+}
+        if ((playerTurn === rock || playerTurn === "r") && computerTurn === rock) {
+            console.log("You chose Rock");
+            console.log("The PC chose Rock");
+            console.log("It's a Draw");
+            
+        } if ((playerTurn === rock || playerTurn === "r") && computerTurn === paper) {
+            console.log("You chose Rock");
+            console.log("The PC chose Paper");
+            console.log("You Lose");
+            
+        } if ((playerTurn === rock || playerTurn === "r") && computerTurn === scissors) {
+            console.log("You chose Rock");
+            console.log("The PC chose Scissors");
+            console.log("You Win");
+            
+        } if ((playerTurn === paper || playerTurn === "p") && computerTurn === rock) {
+            console.log("You chose Paper");
+            console.log("The PC chose Rock");
+            console.log("You Win");
+            
+        } if ((playerTurn === paper || playerTurn === "p") && computerTurn === paper) {
+            console.log("You chose Paper");
+            console.log("The PC chose Paper");
+            console.log("It's a Draw");
+            
+        } if ((playerTurn === paper || playerTurn === "p") && computerTurn === scissors) {
+            console.log("You chose Paper");
+            console.log("The PC chose Scissors");
+            console.log("You Lose");
+            
+        } if ((playerTurn === scissors || playerTurn === "s") && computerTurn === rock) {
+            console.log("You chose Scissors");
+            console.log("The PC chose Rock");
+            console.log("You Lose");
+            
+        } if ((playerTurn === scissors || playerTurn === "s") && computerTurn === paper) {
+            console.log("You chose Scissors");
+            console.log("The PC chose Paper");
+            console.log("You Win");
+            
+        } if ((playerTurn === scissors || playerTurn === "s") && computerTurn === scissors) {
+            console.log("You chose Scissors");
+            console.log("The PC chose Scissors");
+            console.log("It's a Draw");
+            
+        } 
+
